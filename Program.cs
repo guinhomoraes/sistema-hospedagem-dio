28716a1b336eea6d06903c4db4b1059773d26ada
guinhomoraes <guilhermeguilherme98@gmail.com>
2023-09-22 07:45:36 -0300

    Primeiro commit

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿using projetodio_dotnet.Model;
+
+Reserva reserva = new Reserva();
+reserva.HospedeReserva.Nome = "Guilherme";
+reserva.SuiteReserva.NomeSuite = "Presidencial";
+reserva.SuiteReserva.Preco = Convert.ToDouble(100.00);
+reserva.NumeroDias = 5;
+
+double valorReserva = reserva.CalcularReserva();
+
+Console.WriteLine($"Valor da Reserva : R$ {valorReserva}");
